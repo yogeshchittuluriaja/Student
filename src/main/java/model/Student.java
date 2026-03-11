112a0e58344f32e6a1d3c4f8b6665ace1c637f3f
chittuluri yogesh <y.chittuluri@ajacs.in>
2026-03-11 17:25:48 +0530

    annotation

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
